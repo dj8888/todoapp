78f4b22f866702bdfbad40f3b83a97729e356cd1
dj8888 <devansh.jyoti8888@gmail.com>
2020-01-28 01:37:03 +0530

    Add files via upload
    Side nav bar and theme working

--- index.js
+++ index.js
@@ -4,3 +4,36 @@ function toggleSidebar(){
 
 
 } 
+
+
+var checkbox=document.querySelector("input[name=theme]");
+
+checkbox.addEventListener("change",function() {
+    if(this.checked){
+        trans()
+        document.documentElement.setAttribute("data-theme","dark")
+    
+    }
+
+    else{
+        trans()
+        document.documentElement.setAttribute("data-theme","light")
+        
+    }
+
+
+
+}
+
+)
+
+
+let trans = () => {
+    
+    document.documentElement.classList.add("transition");
+    window.setTimeout(() => {
+        document.documentElement.classList.remove("transition")
+    },1000)
+    
+    
+}
